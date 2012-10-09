75358ba19d24716ee3d8b1865b5ced39f63094d6
dbzard <dbzard@gmail.com>
2012-10-09 16:08:57 +0800

    draw grid

--- js/stage.js
+++ js/stage.js
@@ -7,39 +7,27 @@
 		init : function(canvasId) {
 			this.canvas = document.getElementById(canvasId);
 			this.context = this.canvas.getContext('2d');
+
+			var gridWidth = 400;
+			var gridHeight = 400;
+
+			var gridPadding = 10;
+
+			for (var x = 0; x <= gridWidth; x += 40) {
+				this.context.moveTo(0.5 + x + gridPadding, gridPadding);
+				this.context.lineTo(0.5 +x + gridPadding, gridHeight + gridPadding);
+			}
+
+			for (var y = 0; y <= gridHeight; y += 40) {
+				this.context.moveTo(gridPadding, 0.5 + y + gridPadding);
+				this.context.lineTo(gridWidth + gridPadding, 0.5 + y + gridPadding);
+			}
+
+			this.context.strokeStyle = "black";
+			this.context.stroke();
+			
 			console.log('init stage');
 		}
 	};
 
 }());
-
-// //grid width and height
-// var bw = 400;
-// var bh = 400
-// //padding around grid
-// var p = 10;
-// //size of canvas
-// var cw = bw + (p*2) + 1;
-// var ch = bh + (p*2) + 1;
-
-// var canvas = $('<canvas/>').attr({width: cw, height: ch}).appendTo('body');
-
-// var context = canvas.get(0).getContext("2d");
-
-// function drawBoard(){
-//     for (var x = 0; x <= bw; x += 40) {
-//         context.moveTo(0.5 + x + p, p);
-//         context.lineTo(0.5 + x + p, bh + p);
-//     }
-
-
-//     for (var x = 0; x <= bh; x += 40) {
-//         context.moveTo(p, 0.5 + x + p);
-//         context.lineTo(bw + p, 0.5 + x + p);
-//     }
-
-//     context.strokeStyle = "black";
-//     context.stroke();
-// }
-
-// drawBoard();
