900d2a898f33334ec6af83cd6cb347575b65f098
dbzard <dbzard@gmail.com>
2012-10-09 18:34:25 +0800

    add update function to stage

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@
 
 	stage.init('canvas');
 
+	stage.update();
 }());
--- js/stage.js
+++ js/stage.js
@@ -3,30 +3,61 @@
 	window.game.stage = {
 		canvas : null,
 		context : null,
+		matrix : null,
+		gridPadding : null,
+		gridWidth : null,
 
+		// 初始化
 		init : function(canvasId) {
 			this.canvas = document.getElementById(canvasId);
 			this.context = this.canvas.getContext('2d');
+			this.gridPadding = 10;
+			this.gridWidth = 40;
 
+			// 绘制格子
 			var gridWidth = 400;
-			var gridHeight = 400;
+			var gridHeight = 600;
 
-			var gridPadding = 10;
-
-			for (var x = 0; x <= gridWidth; x += 40) {
-				this.context.moveTo(0.5 + x + gridPadding, gridPadding);
-				this.context.lineTo(0.5 +x + gridPadding, gridHeight + gridPadding);
+			for (var x = 0; x <= gridWidth; x += this.gridWidth) {
+				this.context.moveTo(0.5 + x + this.gridPadding, this.gridPadding);
+				this.context.lineTo(0.5 +x + this.gridPadding, gridHeight + this.gridPadding);
 			}
 
-			for (var y = 0; y <= gridHeight; y += 40) {
-				this.context.moveTo(gridPadding, 0.5 + y + gridPadding);
-				this.context.lineTo(gridWidth + gridPadding, 0.5 + y + gridPadding);
+			for (var y = 0; y <= gridHeight; y += this.gridWidth) {
+				this.context.moveTo(this.gridPadding, 0.5 + y + this.gridPadding);
+				this.context.lineTo(gridWidth + this.gridPadding, 0.5 + y + this.gridPadding);
 			}
 
 			this.context.strokeStyle = "black";
 			this.context.stroke();
 			
+			// 初始化矩阵
+			this.matrix = new Array(15);
+			for (var i = 0; i < 15; i++) {
+				this.matrix[i] = new Array(10);
+			}
+
 			console.log('init stage');
+		},
+
+		update : function() {
+			// ------------TODO 删除测试代码-----------------
+			this.matrix[0][0] = true;
+			this.matrix[14][9] = true;
+			// ------------TODO 删除测试代码-----------------
+
+			this.context.fillStyle = "black";
+			var x, y;
+			for (var i = 0; i < 15; i++) {
+				for (var j = 0; j < 10; j++) {
+					if (this.matrix[i][j]) {
+						x = 0.5 + j*this.gridWidth + this.gridPadding;
+						y = 0.5 + i*this.gridWidth + this.gridPadding;
+						this.context.fillRect(x, y, this.gridWidth, this.gridWidth);
+					}
+				}
+			}
+			console.log('update stage');
 		}
 	};
 
