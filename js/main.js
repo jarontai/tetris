b135e10e630cee8bb5c1bc891f426a384aa1953e
dbzard <dbzard@gmail.com>
2012-12-26 21:52:31 +0800

    alert when ie version less than 9.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function() {
+
+	if ($.browser.msie) {
+		var version = parseInt($.browser.version);
+		if (version < 9) {
+			alert("您的IE浏览器不支持HTML5，请升级至IE9或以上版本");
+		}
+	}
+
 	var appView = new AppView();
 });
